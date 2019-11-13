4a7357693cc1f96ffb1466561df0fc7219374fcb
boa50 <bruno.albuquerque50@gmail.com>
2019-11-13 11:40:10 -0300

    criacao de pagination responsivo

--- client/src/components/TrocaMes.js
+++ client/src/components/TrocaMes.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import MediaQuery from 'react-responsive';
 import DateUtils from '../utils/DateUtils';
 
 class TrocaMes extends Component {
@@ -18,17 +19,13 @@ class TrocaMes extends Component {
         }
     }
 
-    isMesInicial = () => {
-        return this._mes === 1;
-    }
+    isMesInicial = () => this._mes === 1;
 
-    isMesFinal = () => {
-        return this._mes === 12;
-    }
+    isMesFinal = () => this._mes === 12;
 
-    isMesAtual = mes => {
-        return mes === this._mes;
-    }
+    isMesAtual = mes => mes === this.getMesAtual();
+
+    getMesAtual = () => this._mes;
 
     botaoSeta = (desabilita, executa, icone) => {
         return (
@@ -51,17 +48,91 @@ class TrocaMes extends Component {
             this.isMesFinal, this.trocaMesPosterior, "chevron_right");
     }
 
+    getMesClasse = mes => {
+        const mesAtual = this.getMesAtual();
+        const condicoesClicabilidade = [
+            mesAtual === 12 && mes === 9,
+            mesAtual === 11 && mes === 9,
+            mesAtual === 1 && mes === 4,
+            mesAtual === 2 && mes === 4,
+            mes === mesAtual + 1,
+            mes === mesAtual - 1
+        ];
+
+        const reducer = (accumulador, valorAtual) => accumulador || valorAtual;
+        const testeClicabilidade = condicoesClicabilidade.reduce(reducer, false);
+
+        if (this.isMesAtual(mes)) {
+            return "active red lighten-1";
+        } else if (this.isMesVisivel(mes)) {
+            return "waves-effect";
+        } else if (testeClicabilidade) {
+            return ""
+        }
+
+        return "mes-nao-apresentado";
+    }
+
+    getMesClasseBigScreen = mes => {
+        if (this.isMesAtual(mes)) {
+            return "active red lighten-1";
+        } else {
+            return "waves-effect";
+        }
+    }
+
+    getMesApresentacao = mes => {
+        if (this.isMesVisivel(mes)) {
+            return (
+                <li className={this.getMesClasse(mes)}
+                    onClick={() => this.trocaMes(mes)}>
+                    <a href="#!">{mes}</a>
+                </li>
+            );
+        }
+
+        return (
+            <li className={this.getMesClasse(mes)}>
+                <a href="#!">...</a>
+            </li>
+        );
+    }
+
+    isMesVisivel = mes => {
+        const mesAtual = this.getMesAtual();
+        const condicoesVisibilidade = [
+            this.isMesAtual(mes),
+            mes === 1 || mes === 12,
+            mesAtual === 12 && (mes === 11 || mes === 10),
+            mesAtual === 11 && mes === 10,
+            mesAtual === 10 && mes === 11,
+            mesAtual === 1 && (mes === 2 || mes === 3),
+            mesAtual === 2 && mes === 3,
+            mesAtual === 3 && mes === 2
+        ];
+
+        const reducer = (accumulador, valorAtual) => accumulador || valorAtual;
+        return condicoesVisibilidade.reduce(reducer, false);
+    }
+
     botoesMeses = () => {
         const meses = DateUtils.getMesesNumeros();
         
         return (
-            meses.map((value) => {
+            meses.map(mes => {
                 return (
-                    <li className={this.isMesAtual(value) ? "active red lighten-1" : "waves-effect"}
-                        onClick={() => this.trocaMes(value)}>
-                        <a href="#!">{value}</a>
-                    </li>
-                )
+                    <Fragment>
+                        <MediaQuery maxDeviceWidth={550}>
+                            {this.getMesApresentacao(mes)}
+                        </MediaQuery>
+                        <MediaQuery minDeviceWidth={550}>
+                            <li className={this.getMesClasseBigScreen(mes)}
+                                onClick={() => this.trocaMes(mes)}>
+                                <a href="#!">{mes}</a>
+                            </li>
+                        </MediaQuery>
+                    </Fragment>
+                );
             })
         )
     }
