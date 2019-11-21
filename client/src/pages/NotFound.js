63a6fa57a96057866f837c79f9e7e6969956b866
boa50 <bruno.albuquerque50@gmail.com>
2019-11-21 09:55:47 -0300

    refatoração da classe NotFound

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from '../components/Header';
 
-function NotFound() {
+const NotFound = () => {
     return (
         <Fragment>
             <Header />
