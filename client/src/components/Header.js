c6c9565d8682e4e461b553db8090519f5833e39c
boa50 <bruno.albuquerque50@gmail.com>
2019-11-21 08:59:51 -0300

    fixação da navbar para o mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import '../assests/custom-css.css';
+import { useMediaQuery } from 'react-responsive';
 
 function Header() {
+    const isBigScreen = useMediaQuery({
+        query: '(min-device-width: 550px)'
+    });
+
     return (
-        <nav>
-            <div className="nav-wrapper red lighten-1">
-                <a href="/" id="logo" className="brand-logo">
-                    <i className="material-icons">cake</i>
-                    Aniversariantes
-                </a>
-            </div>
-        </nav>
+        <div className={isBigScreen ? "" : "navbar-fixed"}>
+            <nav>
+                <div className="nav-wrapper red lighten-1">
+                    <a href="/" id="logo" className="brand-logo">
+                        <i className="material-icons">cake</i>
+                        Aniversariantes
+                    </a>
+                </div>
+            </nav>
+        </div>
     );
 }
 
