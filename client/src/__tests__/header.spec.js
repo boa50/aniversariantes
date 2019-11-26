b75c696f132e0cbe6a34610a5102d04d3bd4ddc0
boa50 <bruno.albuquerque50@gmail.com>
2019-11-26 08:51:07 -0300

    melhoria na cobertura dos testes do header

--- client/src/__tests__/header.spec.js
+++ client/src/__tests__/header.spec.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
-import Header from '../components/Header'
+import Header from '../components/Header';
+import { Context as ResponsiveContext } from 'react-responsive';
 
 let container;
 beforeEach(() => {
@@ -16,12 +17,40 @@ afterEach(() => {
 describe("Header component", () => {
     test("verifica se a renderização foi feita de maneira correta", () => {
         act(() => {
-            ReactDOM.render(<Header/>, container);
+            ReactDOM.render(<Header />, container);
         });
-        const header = container.getElementsByTagName('a')[0];
 
-        expect(header.id).toBe('logo');
-        expect(header.childNodes[0].tagName).toBe('I');
-        expect(header.textContent).toMatch(/Aniversariantes/);
+        const headerBlocoTexto = container.getElementsByTagName('a')[0];
+
+        expect(headerBlocoTexto.id).toBe('logo');
+        expect(headerBlocoTexto.childNodes[0].tagName).toBe('I');
+        expect(headerBlocoTexto.textContent).toMatch(/Aniversariantes/);
+    });
+
+    test("verifica a renderização das diferenças das classes do mobile", () => {
+        const larguraMobile = 549;
+        const larguraDesktop = 550;
+        
+        act(() => {
+            ReactDOM.render(
+                <ResponsiveContext.Provider value={{ width: larguraMobile }}>
+                    <Header />
+                </ResponsiveContext.Provider>
+                , container);
+        });
+
+        const headerMobile = container.getElementsByTagName('div')[0];
+        expect(headerMobile.className).toBe('navbar-fixed');
+
+        act(() => {
+            ReactDOM.render(
+                <ResponsiveContext.Provider value={{ width: larguraDesktop }}>
+                    <Header />
+                </ResponsiveContext.Provider>
+                , container);
+        });
+
+        const headerDesktop = container.getElementsByTagName('div')[0];
+        expect(headerDesktop.className).toBe('');
     });
 });
