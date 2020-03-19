99efcf3900dea946288aed71b1f6f4307117486f
boa50 <bruno.albuquerque50@gmail.com>
2020-03-19 10:23:05 -0300

    Ajustes no trocaMes

--- src/components/trocaMes.js
+++ src/components/trocaMes.js
@@ -1,16 +1,19 @@
 import React from 'react';
+
+import Box from '@material-ui/core/Box';
+
 import Pagination from './pagination';
 
-const TrocaMes = (props) => {
-
+const TrocaMes = props => {
     return (
-        <div className="center">
-            <Pagination 
+        <Box textAlign="center">
+            <Pagination
                 listener={props.listener}
                 page={props.mes}
-                lastPage={12}/>
-        </div>
+                lastPage={12}
+            />
+        </Box>
     );
-}
+};
 
 export default TrocaMes;
