cb370fce15e5979354efd028a167883b8eeceaea
boa50 <bruno.albuquerque50@gmail.com>
2020-03-20 09:33:07 -0300

    Ajustes na listaAniversariantes

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import '../assets/custom-css.scss';
-
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
--- src/components/listaAniversariantes.js
+++ src/components/listaAniversariantes.js
@@ -1,35 +1,57 @@
 import React from 'react';
 import { List } from 'immutable';
 
-const ListaAniversariantes = (props) => {
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+    },
+}));
+
+const ListaAniversariantes = props => {
+    const classes = useStyles();
     const _aniversariantes = props.aniversariantes;
 
-    const imprimeListaVazia = () => <h5>Sem aniversariantes no mês</h5>
+    const imprimeListaVazia = () => (
+        <Typography variant="h5">Sem aniversariantes no mês</Typography>
+    );
 
     const imprimeListaPreenchida = aniversariantes => {
         const aniversariantesOrdenados = ordenarPorDia(aniversariantes);
-        
+
         const linhas = aniversariantesOrdenados.map((linha, index) => {
             return (
-                <tr key={index}>
-                    <td>{linha.pessoa}</td>
-                    <td>{linha.dia}</td>
-                </tr>
+                <TableRow key={index}>
+                    <TableCell>{linha.pessoa}</TableCell>
+                    <TableCell>{linha.dia}</TableCell>
+                </TableRow>
             );
         });
 
         return (
-            <table className="centered hightlight">
-                <tbody>
-                    <tr>
-                        <th>Aniversariante</th>
-                        <th>Dia</th>
-                    </tr>
-                    {linhas}
-                </tbody>
-            </table>
+            <TableContainer className={classes.root} component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Aniversariante</TableCell>
+                            <TableCell>Dia</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>{linhas}</TableBody>
+                </Table>
+            </TableContainer>
         );
-    }
+    };
 
     const ordenarPorDia = aniversariantes => {
         const listaAniversariantesOrdenada = new List(aniversariantes);
@@ -37,13 +59,13 @@ const ListaAniversariantes = (props) => {
         return listaAniversariantesOrdenada._tail.array.sort((a, b) => {
             return a.dia < b.dia ? -1 : 1;
         });
-    }  
+    };
 
     if (_aniversariantes.length === 0) {
         return imprimeListaVazia();
     } else {
         return imprimeListaPreenchida(_aniversariantes);
     }
-}
+};
 
 export default ListaAniversariantes;
