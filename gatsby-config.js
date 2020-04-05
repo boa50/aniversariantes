3ab7aa5a4e9767b18812cb0b25769b331fbd987e
boa50 <bruno.albuquerque50@gmail.com>
2020-04-05 10:45:53 -0300

    Ajustes nos metadados

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Aniversariantes`,
+        description: `Uma aplicação para armazenar os dados de aniversariantes da família.`,
+        author: `@boa50`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
@@ -46,11 +46,9 @@ module.exports = {
                 background_color: `#ffffff`,
                 theme_color: `#000000`,
                 display: `minimal-ui`,
-                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+                icon: `src/images/favicon.png`,
             },
         },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
         `gatsby-plugin-offline`,
     ],
 };
