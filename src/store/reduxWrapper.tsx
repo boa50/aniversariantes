312279a78ec2a3904ac7b3fe172f9fad69561d99
boa50 <bruno.albuquerque50@gmail.com>
2020-04-16 11:22:16 -0300

    Correções na build

--- src/store/reduxWrapper.tsx
+++ src/store/reduxWrapper.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import aniversariantesReducer from './reducers/aniversariantes';
 import authReducer from './reducers/auth';
 import { watch } from './sagas';
-
-declare global {
-    interface Window {
-        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
-}
-
-const composeEnhancers =
-    (process.env.NODE_ENV === 'development'
-        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-        : null) || compose;
 
 const rootReducer = combineReducers({
     aniversariantes: aniversariantesReducer,
@@ -25,10 +14,7 @@ const rootReducer = combineReducers({
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(sagaMiddleware)),
-);
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(watch);
 
