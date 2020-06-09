9b76d965e7779b4f39135d0ac646ebb01a4f0f8c
boa50 <bruno.albuquerque50@gmail.com>
2020-06-09 09:00:06 -0300

    Configurações de variáveis de ambiente

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: `https://firestore.googleapis.com/v1/projects/${process.env.PROJECT}/databases/(default)/documents/familias/`,
+    baseURL: `https://firestore.googleapis.com/v1/projects/${process.env.GATSBY_PROJECT}/databases/(default)/documents/familias/`,
 });
 
 export default instance;
